dc28f0d95d35ae4540ef537428f7fd5a53fac17f
Plot <49232758+Plot1337@users.noreply.github.com>
2023-07-15 21:19:38 +1000

    Create HClientFactory.cs

---
+++ HttpClientFactory/HClientFactory.cs
@@ -0,0 +1,18 @@
+﻿using System.Net;
+
+namespace HttpClientFactory;
+
+public class HClientFactory : IHttpClientFactory
+{
+    private readonly Func<HttpClientHandler> _makeHandler;
+
+    public HClientFactory(Func<HttpClientHandler> makeHandler) =>
+        _makeHandler = makeHandler;
+
+    public HttpClient CreateClientWithProxy(IWebProxy webProxy)
+    {
+        var handler = _makeHandler();
+        handler.Proxy = webProxy;
+        return new(handler, true);
+    }
+}
