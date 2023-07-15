7c4823eec3b2af472806842ab3452b766bfb5c8d
Plot <49232758+Plot1337@users.noreply.github.com>
2023-07-15 21:19:40 +1000

    Create CachedHttpClientFactory.cs

---
+++ HttpClientFactory/CachedHttpClientFactory.cs
@@ -0,0 +1,30 @@
+﻿using System.Net;
+
+namespace HttpClientFactory;
+
+public class CachedHttpClientFactory : IHttpClientFactory
+{
+    private readonly IHttpClientFactory _httpClientFactory;
+    private readonly Dictionary<int, HttpClient> _cache = new();
+
+    public CachedHttpClientFactory(IHttpClientFactory httpClientFactory) =>
+        _httpClientFactory = httpClientFactory;
+
+    public HttpClient CreateClientWithProxy(IWebProxy webProxy)
+    {
+        var key = webProxy.GetHashCode();
+
+        lock (_cache)
+        {
+            if (_cache.ContainsKey(key))
+                return _cache[key];
+
+            var result = _httpClientFactory.CreateClientWithProxy(
+                webProxy
+                );
+
+            _cache.Add(key, result);
+            return result;
+        }
+    }
+}
